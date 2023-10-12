f466ca2ad3edc36679d078e395cc63b581ece4f2
OwenDelBene <owen.delbene@gmail.com>
2023-10-12 16:32:25 -0500

    new stm

--- tlt_test.py
+++ tlt_test.py
@@ -46,6 +46,54 @@ def dynamics(t, state):
         -rho[2]*(1-mu)/d**3 - mu*rho[2]/r**3
         ])
 
+def stm(state):
+    rho = state[:3]
+    mu= m_moon/(m_earth + m_moon)
+    d= np.sqrt((rho[0]+mu)**2 + rho[1]**2 + rho[2]**2)#r13/lstar
+    r= np.sqrt((rho[0]-1+mu)**2 + rho[1]**2 + rho[2]**2)#r23/lstar
+    #d=sqrt((x+mu)**2 + y**2 + z^^2)
+    #r=sqrt((x-1+mu)**2 + y**2 + z**2)
+    x = state[0]
+    y=state[1]
+    z=state[2]
+    xd=state[3]
+    yd=state[4]
+    zd=state[5]
+    #dx../dx = 
+    #1 - d (x+mu-mux -mu**2)/d**3 + d (-mux+mu+mu**2)/r**3
+    dxdx1 = 1 - ((-mu+1)*(y**2+z**2-2*(x+mu)**2)) / (y**2+z**2 + (x+mu)**2)**(5/2) - (mu*(y**2+z**2-2*(x+mu-1)**2)) / (y**2+z**2 + (x+mu-1)**2)**(5/2) 
+    dxdx2 = (3*y*(-mu+1)*(x+mu)) / (y**2+z**2+(x+mu)**2)**(5/2) + (3*y*(mu**2 + x*mu-mu)) / (y**2+z**2+(x+mu-1)**2)**(5/2)
+    dxdx3 = (3*z*(-mu+1)*(x+mu)) / (y**2+z**2+(x+mu)**2)**(5/2) + (3*z*(mu**2 +x*mu - mu)) / (y**2+z**2+(x+mu-1)**2)**(5/2)
+    dxdx4 = 0
+    dxdx5 = 2
+    dxdx6 = 0
+
+    dydx1 = (3*y*(-mu+1)*(mu+x)) / (y**2+z**2+(mu+x)**2)**(5/2) + (3*y*mu*(mu+x-1)) / ((x-1+mu)**2+y**2+z**2)**(5/2)
+    dydx2 = 1 - ((1-mu)*(mu**2+2*mu*x+x**2+z**2-2*y**2)) / (y**2+z**2 + (mu+x)**2)**(5/2) - (mu*(mu**2+2*mu*x-2*mu+x**2+z**2+1-2*y**2-2*x)) / (y**2+z**2 + (mu+x-1)**2)**(5/2)
+    dydx3 = (3*y*mu*z) / (y**2+z**2+(mu+x-1)**2)**(5/2) + (3*y*z*(1-mu)) / (y**2+z**2+(mu+x)**2)**(5/2)
+    dydx4 = -2
+    dydx5 = 0
+    dydx6 = 0
+
+    dzdx1 = (3*z*(1-mu)*(x+mu)) / ((x+mu)**2+y**2+z**2)**(5/2) + (3*mu*z*(x+mu-1)) / ((x-1+mu)**2+y**2+z**2)**(5/2)
+    dzdx2 = (3*z*y*(1-mu)) / ((x+mu)**2+y**2+z**2)**(5/2) + (3*mu*z*y) / ((x-1+mu)**2+y**2+z**2)**(5/2)
+    dzdx3 = -(mu**2+2*mu*x+x**2+y**2-2*z**2)*(1-mu) / ((mu+x)**2+y**2+z**2)**(5/2) - mu*(-2*z**2+mu**2-2*mu+x**2+2*mu*x-2*x+y**2+1) / ((mu+x-1)**2+y**2+z**2)**(5/2)
+    dzdx4=0
+    dzdx5=0
+    dzdx6=0
+
+
+    stm = np.array([
+        [0,0,0,1,0,0],
+        [0,0,0,0,1,0],
+        [0,0,0,0,0,1],
+        [dxdx1,dxdx2,dxdx3,dxdx4,dxdx5,dxdx6],
+        [dydx1,dydx2,dydx3,dydx4,dydx5,dydx6],
+        [dzdx1,dzdx2,dzdx3,dzdx4,dzdx5,dzdx6]
+        ])
+    return stm
+
+
 def propogate(state, t, dt, tf):
     '''
     propogate dynamics given initial state at inital time
@@ -115,6 +163,8 @@ if __name__ == "__main__":
     
 
     print(f'p1:: {prop[-1][:3]}, pd: {pd}')
+    stm1 = stm(state_guess)
+    print('STM WORKED!', stm1)
     while residual > epsilon:
         print('residual', residual)
         rho1 = state_guess[:3]
@@ -123,6 +173,9 @@ if __name__ == "__main__":
         
         #compute jacobian using finite difference
         h=.001* tstar / lstar
+   
+        #dp1/dv0
+        '''
         b1 = ((propogate(state_guess + np.array([0,0,0,h, 0,0]),0,dt,tf )[-1] - propogate(state_guess - np.array([0,0,0,h, 0,0]),0,dt,tf ))[-1] / (2*h))[:3]
         b2 = ((propogate(state_guess + np.array([0,0,0,0, h,0]),0,dt,tf )[-1] - propogate(state_guess - np.array([0,0,0,0, h,0]),0,dt,tf ))[-1] / (2*h))[:3]
         b3 = ((propogate(state_guess + np.array([0,0,0,0, 0,h]),0,dt,tf )[-1] - propogate(state_guess - np.array([0,0,0,0, 0,h]),0,dt,tf ))[-1] / (2*h))[:3]
@@ -132,14 +185,16 @@ if __name__ == "__main__":
             b2,
             b3
             )).T
+        '''
+        B = stm(state_guess)[:3, 3:]
         print('b:', B) 
-        DX = np.matmul(-np.linalg.inv(B),rho1-pd)# rho1-pd*0)
+        DX = np.matmul(-np.linalg.inv(B),-rho1+pd)# rho1-pd*0)
         state_guess[3:] += DX 
         print(f'DX: {DX}')
         p1 = propogate(state_guess, 0, dt, tf)
         
         plot_trajectory(p1*lstar) 
-        print(f'p1:: {p1[-1][:3]}, pd: {pd}')
+        print(f'p1: {p1[-1][:3]}, pd: {pd}')
         
         residual = np.linalg.norm( p1[-1][:3]- pd) * lstar 
 #        print('iteration',residual )
